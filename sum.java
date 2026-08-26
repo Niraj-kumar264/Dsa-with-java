72ace832a8541ab54ff34de74a483b20abadca39
Niraj kumar <roydhiraj.7350@gmail.com>
2026-08-26 17:15:19 +0530

    basic sum programme

---
+++ sum.java
@@ -0,0 +1,24 @@
+public class sum {
+ public static void main(String[] args) {
+    int a = 12;
+    int b = 8;
+    int c = a+b;
+        System.out.print("sum = ");
+    System.out.println(c);
+    int diff = a - b ;
+    System.out.print("difference is " );
+    System.out.println(diff);
+    int multi = a * b;
+    System.out.print("multiplication = ");
+    System.out.println(multi);
+    int div = a/b ;
+    System.out.print("division =");
+System.out.println(div);
+int cal = a*b / a-b;
+System.out.println(cal);
+int cal1 = (a*b)/(a-b);
+System.out.println(cal1);
+
+ }
+    
+}
